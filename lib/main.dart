461213063f032ab119e1dd419176aac1b3ee717d
changuk2 <changuk0308@naver.com>
2024-01-20 15:11:50 +0900

    Lecture:Data Fetching(API호출)및 app bar

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:mini_webtoon_app/screens/home_screen.dart';
 import 'package:mini_webtoon_app/services/api_service.dart';
 
 void main() {
+  //내가 만든 ApiService 클래스 중 .getTodaysToons() 함수 사용
   ApiService().getTodaysToons(); //★★★둘다 "()"를 붙여야함.★★★
   runApp(const App());
 }
