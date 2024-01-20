bda10a8277c26e1bd26b61641525d75de608197e
changuk2 <changuk0308@naver.com>
2024-01-20 16:28:28 +0900

    feat: pud.dev의 http 패키지로 API호출한 후 리스트로 관리하기 Data fetching,async programming and fromJson

---
+++ lib/models/webtoon_model.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+//각 웹툰별 초기화 해주는 클래스
+class WebtoonModel {
+  final String id, thumb, title;
+  //★★★named Constructor★★★
+  /* 1.getTodaysToons()함수의 일부(dynamic형태로 줌.)
+    final List<dynamic> Webtoons = jsonDecode(response.body);
+    for (var webtoon in Webtoons) {
+        WebtoonModel.fromJson(webtoon);
+    }
+  */
+  //2. named Constructor의 이름을 fromJson이라고 지음.
+  //3. 각각 <키(특성),data(dynamic)> 쌍으로 json을(객체) 받음. 따라서 Map 사용
+  WebtoonModel.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        thumb = json['thumb'],
+        title = json['title'];
+}
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,17 +1,22 @@
+import 'dart:convert';
 //★★★http패키를 불러오되,해당 패키지에 namespace를 지정함.★★★
 //★★★"as http"를 통해 패키지의 이름을 간단하게 구성한다.★★★
-import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
+import 'package:mini_webtoon_app/models/webtoon_model.dart';
 
 class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodaysToons() async {
+  //List<WebtoonModel> 형태로 반환 하는데 비동기 이므로 Future로 반환
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    //json으로 웹툰을 받아올때마다 List에 추가하기
+    List<WebtoonModel> webtoonInstances = [];
+
     //★★get(Uri url, {Map<String, String>? headers})★★
     //★★url은 Uri의 형태로 받는다.★★
     final url = Uri.parse('$baseUrl/$today');
-    //★Future<Response>이 형태로 받는 이유: 어떤 일이 일어날때까지 기다리는 것
+    //★★★★Future<Response>이 형태로 받는 이유: 어떤 일이 일어날때까지 기다리는 것
     //★get함수가 미래에 완료되면 서버에 대한 응답도 함.
     //★★★우리는 이것을 비동기(async) 프로그램이라고도 한다.★★★
     // await http.get(url); //해당 값을 받아오기 까지 기다리기 위해★★★ "await" 지시어 사용
@@ -19,8 +24,20 @@ class ApiService {
     final response = await http.get(url);
     //응답 코드가 200이면 성공한거임.
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      //★★★★★jsonDecode(response.body): 그저 텍스트로 된 body문장들을
+      //★★★★★dynamic(어떤 데이터값이든 상관x)형태로 반환한다.
+      //★★★이때 Json데이터는 {}(오브젝트)들로 구성된 List형태이므로
+      //★★★해당 객체 webtoons의 데이터 타입을 List<dynamic>으로 받는다.
+      final List<dynamic> Webtoons = jsonDecode(response.body);
+      for (var webtoon in Webtoons) {
+        /*웹툰 제목 출력 WebtoonModel.fromJson(webtoon) -> named constructor에서 초기화 시켜줌.
+        final toon = WebtoonModel.fromJson(webtoon);
+        print(toon.title);
+        */
+        //리스트에 넣고 관리하기
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+      }
+      return webtoonInstances;
     }
     //응답 코드가 200이 아니면 실패한 거임.
     throw Error();
